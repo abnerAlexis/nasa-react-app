f85fb1c3a5532209e08086dba698398d7c2ca4f9
Alexis Abner <alexiproj@gmail.com>
2024-10-05 14:37:55 -0600

    update vite.congig.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // This should be the same as in netlify.toml
+  },
+});
